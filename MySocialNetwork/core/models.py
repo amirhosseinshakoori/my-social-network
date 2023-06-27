eac468a701fa3979ee8675ba5245b26ed8d9fa82
amirhosseinshakoori <amirhossein.shakoori@yahoo.com>
2023-06-27 13:41:53 +0330

    conect profile to models

--- MySocialNetwork/core/models.py
+++ MySocialNetwork/core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 
 class Profile(models.Model):
@@ -8,3 +9,12 @@ class Profile(models.Model):
 
     def __str__(self) -> str:
         return self.user.username
+    
+
+
+def create_profile(sender, instance, created, **kwarge):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+
+post_save.connect(create_profile,sender=User)
