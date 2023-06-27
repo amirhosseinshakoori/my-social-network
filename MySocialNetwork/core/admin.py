08cbf3680c5f56c6c4ac7cfe4beb37ed641560d2
amirhosseinshakoori <amirhossein.shakoori@yahoo.com>
2023-06-27 12:34:41 +0330

    add profile model

--- MySocialNetwork/core/admin.py
+++ MySocialNetwork/core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group , User
+from .models import Profile
 
+admin.site.unregister(Group)
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ["Username"]
+
+admin.site.unregister(User)
+admin.site.register(User,UserAdmin)
+
+class Profileinline(admin.StackedInline):
+    model = Profile
