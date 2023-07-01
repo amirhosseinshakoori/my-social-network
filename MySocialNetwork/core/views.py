5393d648d10da43475ab41a8c45a52599831cdb5
amirhosseinshakoori <amirhossein.shakoori@yahoo.com>
2023-07-01 19:57:13 +0330

    add Tweetform to home viwe

--- MySocialNetwork/core/views.py
+++ MySocialNetwork/core/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render ,redirect
 from django.contrib import messages
 from .models import Profile ,Tweet
-
+from .forms import TweetForm
 # Create your views here.
 
-def home (request):
-    if request.user.is_authenticated:
-
-     Tweets = Tweet.objects.all().order_by("-created_at")
+def home(request):
+	if request.user.is_authenticated:
+		form = TweetForm(request.POST or None)
+		if request.method == "POST":
+			if form.is_valid():
+				Tweet = form.save(commit=False)
+				Tweet.user = request.user
+				Tweet.save()
+				messages.success(request, ("Your Tweet Has Been Posted!"))
+				return redirect('home')
 		
-		
-     return render(request, 'home,html',{})
+		Tweets = Tweet.objects.all().order_by("-created_at")
+		return render(request, 'home.html', {"Tweets":Tweets, "form":form})
+	else:
+		Tweets = Tweet.objects.all().order_by("-created_at")
+		return render(request, 'home.html', {"Tweets":Tweets})
 
 def profile_list(request):
 	if request.user.is_authenticated:
