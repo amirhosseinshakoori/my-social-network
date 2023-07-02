ca8052b62bac7b0dbefea4dc7d816509a3cfe3de
amirhosseinshakoori <amirhossein.shakoori@yahoo.com>
2023-07-02 12:25:37 +0330

    add register to my app

--- MySocialNetwork/core/views.py
+++ MySocialNetwork/core/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render ,redirect
 from django.contrib import messages
 from .models import Profile ,Tweet
-from .forms import TweetForm
+from .forms import TweetForm ,SignUpForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.models import User
+
+
 # Create your views here.
 
 def home(request):
@@ -50,3 +56,43 @@ def profile(request, pk):
 	else:
 		messages.success(request, ("You Must Be Logged In To View This Page..."))
 		return redirect('home')	
+	
+
+def login_user(request):
+	if request.method == "POST":
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			messages.success(request, ("You Have Been Logged In!  Get TweetING!"))
+			return redirect('home')
+		else:
+			messages.success(request, ("There was an error logging in. Please Try Again..."))
+			return redirect('login')
+
+	else:
+		return render(request, "login.html", {})
+
+
+def logout_user(request):
+	logout(request)
+	messages.success(request, ("You Have Been Logged Out. Sorry to Tweet You Go..."))
+	return redirect('home')
+
+def register_user(request):
+	form = SignUpForm()
+	if request.method == "POST":
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+
+			user = authenticate(username=username, password=password)
+			login(request,user)
+			messages.success(request, ("You have successfully registered! Welcome!"))
+			return redirect('home')
+	
+	return render(request, "register.html", {'form':form})
+
